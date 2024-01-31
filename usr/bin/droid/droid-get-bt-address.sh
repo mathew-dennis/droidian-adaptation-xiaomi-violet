b91be184c72797f43c1fadc33f9d0a06e51b7dc0
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2024-01-31 17:56:53 +0530

    bluetooth: Import script from op3 to load the proper bluetooth address
    This script is orginally by @FakeShell done for op3

---
+++ usr/bin/droid/droid-get-bt-address.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# On the Redmi Note 7 Pro, /var/lib/bluetooth contains a directory with the bluetooth device's board address as it's name,
+# so get that value and put it in /var/lib/bluetooth/board-address.
+find /var/lib/bluetooth -maxdepth 1 -iname '*:*:*:*:*:*' | cut -d/ -f 5 > /var/lib/bluetooth/board-address
+chown root:root /var/lib/bluetooth/board-address
+chmod 644 /var/lib/bluetooth/board-address
+
